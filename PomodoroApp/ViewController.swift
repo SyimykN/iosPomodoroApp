706ccca44bcb61cf87d943bd33a772231e2b4ef8
SemaDevpy <syimykdev@gmail.com>
2020-07-04 17:33:31 +0600

    Initial Commit

---
+++ PomodoroApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PomodoroApp
+//
+//  Created by Syimyk on 7/4/20.
+//  Copyright © 2020 Syimyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
