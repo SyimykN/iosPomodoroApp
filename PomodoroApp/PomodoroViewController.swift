bc02077deaa36019f023cb3912bd861786f5fcf8
SemaDevpy <syimykdev@gmail.com>
2020-08-12 15:26:22 +0600

    ready app

---
+++ PomodoroApp/PomodoroViewController.swift
@@ -0,0 +1,167 @@
+//
+//  PomodoroViewController.swift
+//  PomodoroApp
+//
+//  Created by Syimyk on 7/5/20.
+//  Copyright © 2020 Syimyk. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PomodoroViewController: UIViewController {
+    var audioPlayer = AVAudioPlayer()
+    var timer = Timer()
+    var isTimerStarted = false
+    
+    
+    @IBOutlet weak var ShortBTN: UIButton!
+    @IBOutlet weak var LongBTN: UIButton!
+    @IBOutlet weak var PomodoroBTn: UIButton!
+    
+    
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    
+    var settedPomodoro : Int?
+    var settedShortBreak : Int?
+    var settedLongBreak : Int?
+    
+    
+    var time = 0
+    var saveTime = 0
+    
+    
+    @IBOutlet weak var clock: UILabel!
+    
+    @IBOutlet weak var startBTN: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        do{
+            let audioPath = Bundle.main.path(forResource: "alarm_sound", ofType: ".mp3")
+            try audioPlayer = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+        }catch{
+            print("\(error)")
+        }
+        
+        clock.text = "\(settedPomodoro!) : 00"
+        if let savePomodoro = settedPomodoro{
+            time = savePomodoro * 60
+            saveTime = savePomodoro * 60
+        }
+    }
+    
+    
+    @IBAction func startPressed(_ sender: Any) {
+        if startBTN.currentTitle == "Start"{
+            if !isTimerStarted{
+                startTimer()
+                isTimerStarted = true
+                startBTN.setTitle("Stop", for: .normal)
+            }else{
+                timer.invalidate()
+                isTimerStarted = false
+            }
+        }else{
+            timer.invalidate()
+            isTimerStarted = false
+            startBTN.setTitle("Start", for: .normal)
+        }
+    }
+    
+    
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer(){
+        if time > 0{
+        time -= 1
+        clock.text = formatTime()
+        }else{
+            timer.invalidate()
+            audioPlayer.play()
+            clock.text = formatTime()
+            time = saveTime
+            startBTN.setTitle("Start", for: .normal)
+            
+            //
+        }
+    }
+    
+    func formatTime() -> String{
+        if time > 0{
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format: "%02i:%02i", minutes, seconds)
+    }else{
+        let minutes = Int(saveTime) / 60 % 60
+        let seconds = Int(saveTime) % 60
+        return String(format: "%02i:%02i", minutes, seconds)
+    }
+    }
+    
+    
+//MARK: - ShortButtonPressed
+    
+    @IBAction func shortBreakPressed(_ sender: UIButton) {
+        clock.text = "\(settedShortBreak!) : 00"
+        if let saveShort = settedShortBreak{
+                   time = saveShort * 60
+                   saveTime = saveShort * 60
+               }
+        view.backgroundColor = UIColor.cyan
+        
+        
+        quoteLabel.text = "Time for a break"
+        
+        ShortBTN.backgroundColor = .black
+        PomodoroBTn.backgroundColor = UIColor(red: 321, green: 322, blue: 123, alpha: 0.0)
+        LongBTN.backgroundColor = UIColor(red: 321, green: 322, blue: 123, alpha: 0.0)
+    }
+  
+    
+    
+ //MARK: - LongButtonPressed
+    
+    @IBAction func LongBreakBtnPressed(_ sender: UIButton) {
+        clock.text = "\(settedLongBreak!) : 00"
+        if let saveLong = settedLongBreak{
+                   time = saveLong * 60
+                   saveTime = saveLong * 60
+               }
+        view.backgroundColor = UIColor.green
+        
+        
+        quoteLabel.text = "Time for a break"
+        LongBTN.backgroundColor = .black
+        PomodoroBTn.backgroundColor = UIColor(red: 251, green: 232, blue: 211, alpha: 0.0)
+        ShortBTN.backgroundColor = UIColor(red: 251, green: 232, blue: 211, alpha: 0.0)
+    }
+    
+    //MARK: - pomodoroBTNPressed
+    
+    @IBAction func PomodoroBtnPressed(_ sender: Any) {
+        clock.text = "\(settedPomodoro!) : 00"
+        if let savePomodoro = settedPomodoro{
+                   time = savePomodoro * 60
+                   saveTime = savePomodoro * 60
+               }
+        view.backgroundColor = UIColor.systemBlue
+        
+        
+        quoteLabel.text = "Time To Study"
+        PomodoroBTn.backgroundColor = .black
+        LongBTN.backgroundColor = UIColor(red: 251, green: 232, blue: 211, alpha: 0.0)
+        ShortBTN.backgroundColor = UIColor(red: 251, green: 232, blue: 211, alpha: 0.0)
+    }
+    
+    
+
+//MARK: - Helping Method to Reset Timer When Its Done
+    
+}
--- PomodoroApp/ViewController.swift
+++ PomodoroApp/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var PomodoroSet: UITextField!
+    @IBOutlet weak var ShortSet: UITextField!
+    @IBOutlet weak var LongSet: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func goBTNtapped(_ sender: UIButton) {
+       
+    }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+          // Get the new view controller using segue.destination
+        var destinationVC = segue.destination as! PomodoroViewController
+        destinationVC.settedPomodoro = Int(PomodoroSet.text!)
+        destinationVC.settedShortBreak = Int(ShortSet.text!)
+        destinationVC.settedLongBreak = Int(LongSet.text!)
+        
+          // Pass the selected object to the new view controller.
+      }
+    
 }
+
 
